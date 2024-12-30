fde87eac0f517c28f786bd9be9874cc320c08418
Abdulaziz Ghuloum <aghuloum@gmail.com>
2024-12-30 05:32:14 +0300

    added expect test

--- test-project/.rts/test1.test.rts.ts
+++ test-project/.rts/test1.test.rts.ts
@@ -1,4 +1,6 @@
-import { suite as suite_1, test as test_2 } from "vitest";
+import { suite as suite_1, test as test_2, expect as expect_3 } from "vitest";
 suite_1("sample test", () => {
-  test_2("should work", () => {});
+  test_2("should work", () => {
+    expect_3(5).toBe(5);
+  });
 });
